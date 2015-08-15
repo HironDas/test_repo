007b4bd7845c76f5273fb637de9e0c2acb08af67
hirondas <hiron_cse@yahoo.com>
2015-08-15 18:57:32 +0000

    the server is edited for c9io

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var http = require('http');
 
+var port = process.env.PORT;
+var host = process.env.IP;
+
 http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type' :"text/type"});
 	res.end('Hello World\n');
-}).listen(1337);
+}).listen(port, host);
 
 console.log("server running!");
 
