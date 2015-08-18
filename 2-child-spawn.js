4c426657f834361adf44543a99ce87b7f677a066
hirondas <hiron_cse@yahoo.com>
2015-08-18 13:31:22 +0000

    the last commit

---
+++ 2-child-spawn.js
@@ -0,0 +1,14 @@
+var spawn = require("child_process").spawn;
+var ps = spawn('ps', ['ax']);
+var grep = spawn('grep', ['node']);
+
+ps.stdout.pipe(grep.stdin);
+grep.stdout.pipe(process.stdout);
+
+ps.stderr.on('data', function (data) {
+    console.log("ps stderr: "+data);
+});
+
+grep.stderr.on('data', function(data){
+    console.log('grep stderr: '+data);
+});
